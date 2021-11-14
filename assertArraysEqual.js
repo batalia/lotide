477e7c65cdc5533b1c74adcf99cb4a8002f5f012
Natalia <batalia.nerdys@gmail.com>
2021-11-14 18:55:51 -0500

    Refactor

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,7 +1,7 @@
 const array1 = process.argv[2];
 const array2 = process.argv[3];
 
-const eqArrays = (array1, array2) => {
+const assertArraysEqual = (array1, array2) => {
   for (let i = 0; i < array1.length; i++) {
     if (array1[i] !== array2[i]) {
       console.log(`🛑🛑🛑 Assertion Failed: ${array1} === ${array2}`);
@@ -10,4 +10,4 @@ const eqArrays = (array1, array2) => {
   console.log(`✅✅✅ Assertion Passed: ${array1} !== ${array2}`);
 };
 
-eqArrays(array1, array2);
+assertArraysEqual(array1, array2);
