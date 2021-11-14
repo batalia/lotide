d7f9b06a8efe5b6da9844700dbc411ebba709360
Natalia <batalia.nerdys@gmail.com>
2021-11-14 18:50:09 -0500

    Added and implemented assertArraysEqual

---
+++ assertArraysEqual.js
@@ -0,0 +1,13 @@
+const array1 = process.argv[2];
+const array2 = process.argv[3];
+
+const eqArrays = (array1, array2) => {
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] !== array2[i]) {
+      console.log(`🛑🛑🛑 Assertion Failed: ${array1} === ${array2}`);
+    }
+  }
+  console.log(`✅✅✅ Assertion Passed: ${array1} !== ${array2}`);
+};
+
+eqArrays(array1, array2);
