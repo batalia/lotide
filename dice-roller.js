6f588717b1e6cee3ef90ae5f7f11de811b0c05ba
Natalia <batalia.nerdys@gmail.com>
2021-11-14 18:19:05 -0500

    Linted

--- dice-roller.js
+++ dice-roller.js
@@ -1,17 +1,17 @@
 // Add code to get number from user input
-const num = process.argv[2]
+const num = process.argv[2];
 
-function getRandomInt(max) {
-    return Math.floor(Math.random() * max);
-  }
+const getRandomInt = function(max) {
+  return Math.floor(Math.random() * max);
+};
 
-function diceRoller(num) {
-    const numberArray = []
-    // Add functionality to get num from args, create 3 random numbers between 1 - 6, and return a string
-    for (i = 0; i < num; i++) {
-        numberArray.push(getRandomInt(6)+1);
-    }
-    return `Rolled ${num} dice: ${numberArray.join(', ')}`;
+const diceRoller = function(num) {
+  const numberArray = [];
+  // Add functionality to get num from args, create 3 random numbers between 1 - 6, and return a string
+  for (let i = 0; i < num; i++) {
+    numberArray.push(getRandomInt(6) + 1);
   }
+  return `Rolled ${num} dice: ${numberArray.join(', ')}`;
+};
   
-  console.log(diceRoller(num)); // Num represents the number received from the args
+console.log(diceRoller(num)); // Num represents the number received from the args
