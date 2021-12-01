ea12bbfbe67538292490772d7acb97b7991b37b0
Natalia <batalia.nerdys@gmail.com>
2021-11-30 20:08:46 -0500

    Refactor

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,12 +1,3 @@
-// const assertArraysEqual = require('../assertArraysEqual');
-// const middle = require('../middle');
-
-// //TEST
-// assertArraysEqual([2], middle([1, 2, 3]));
-// assertArraysEqual([2, 3], middle([1, 2, 3, 4]));
-// assertArraysEqual([], middle([]));
-// assertArraysEqual([], middle([1]));
-
 const assert = require('chai').assert;
 const middle   = require('../middle');
 
